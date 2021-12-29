4536fafdbf4ede046c77e819f5286aa8456b8903
miku8323 <takemasa.wadai@gmail.com>
2021-12-30 02:53:11 +0900

    クリア判定

---
+++ Assets/Scripts/BreakOutManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakOutManager : MonoBehaviour
+{
+
+    GameObject[] blocks = new GameObject[36];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // block生成
+        // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject sphere_prefab = Resources.Load<GameObject>("Block");
+        for(int j = 0; j < 4; j++)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                blocks[j*9+i] = Instantiate(sphere_prefab);
+                blocks[j*9+i].transform.position = new Vector3(-6.17f + 0.5f * j, 0.3f, -4.0f + 1.0f * i);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 終了判定
+        if(DestroyAllBlocks())
+        {
+            // クリア
+            Debug.Log("クリア");
+        }
+    }
+
+    private void setBlock()
+    {
+
+    }
+
+    // 終了判定
+    private bool DestroyAllBlocks()
+    {
+        foreach(GameObject b in blocks)
+        {
+            if(b != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // ボタン押したら早送り
+}
